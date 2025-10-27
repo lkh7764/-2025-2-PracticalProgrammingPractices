e65a828409a88642d6a6db30fb5c2f519f9d890b
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2025-10-27 22:33:29 +0900

    20251027_addMP4

---
+++ [2025-2]UnityServerClientWithMultithread/Assets/Scripts/Legacy/FileManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileManager : LockSingleton<AccountManager>
+{
+    private static string filePath = "savedData.json";
+
+
+
+}
